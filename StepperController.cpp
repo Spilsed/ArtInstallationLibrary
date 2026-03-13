2dbbf4ee684c57c5c522e9e56309353d40bf47f9
Spilsed <93678295+Spilsed@users.noreply.github.com>
2026-03-13 16:39:39 -0400

    Add enable and microstepping to StepperController

--- StepperController.cpp
+++ StepperController.cpp
@@ -1,15 +1,20 @@
 #include "StepperController.h"
+#include "Utils.h"
 #include <thread>
 #include <chrono>
 
 using namespace std::chrono;
 
-StepperController::StepperController(unsigned int step_pin, 
-                                     unsigned int dir_pin, 
+StepperController::StepperController(unsigned int step_pin,
+                                     unsigned int dir_pin,
+                                     unsigned int enable_pin,
+                                     unsigned int microstep_pins[4],
                                      const std::string &chip_path)
     : _chip(chip_path),
       _step_offset(step_pin),
       _dir_offset(dir_pin),
+      _enable_offset(enable_pin),
+      _microstep_offsets(microstep_pins),
 
       _request(_chip.prepare_request()
                 .set_request_config(gpiod::request_config().set_consumer("stepper_ctrl"))
@@ -38,5 +43,21 @@ void StepperController::move(int steps, bool clockwise, int delay_us) {
         
         _request.set_value(_step_offset, gpiod::line::value::INACTIVE);
         std::this_thread::sleep_for(delay);
+
+        delay_us = Utils::lerp(delay_us, delay_us, 1);
+        delay = microseconds(delay_us);
     }
+}
+
+void StepperController::setMicrostep(int value) {
+
+}
+
+void StepperController::setEnabled(bool value) {
+    _enabled = value;
+    _request.set_value(_dir_offset, value ? gpiod::line::value::INACTIVE : gpiod::line::value::ACTIVE);
+}
+
+bool StepperController::isEnabled() {
+    return _enabled;
 }
--- StepperController.h
+++ StepperController.h
@@ -4,13 +4,27 @@
 #include <string>
 
 class StepperController {
-public:
-    StepperController(unsigned int step_pin, unsigned int dir_pin, const std::string &chip_path);
-    void move(int steps, bool clockwise, int delay_us);
+    public:
+        StepperController(
+            unsigned int step_pin,
+            unsigned int dir_pin,
+            unsigned int enable_pin,
+            unsigned int microstep_pins[4],
+            const std::string &chip_path
+        );
+        void move(int steps, bool clockwise, int delay_us);
+        void setMicrostep(int value);
+        void setEnabled(bool value);
 
-private:
-    gpiod::chip _chip;
-    unsigned int _step_offset;
-    unsigned int _dir_offset;
-    gpiod::line_request _request;
+        bool isEnabled();
+
+    private:
+        gpiod::chip _chip;
+        unsigned int _step_offset;
+        unsigned int _dir_offset;
+        unsigned int _enable_offset;
+        unsigned int *_microstep_offsets;
+        gpiod::line_request _request;
+
+        bool _enabled;
 };
